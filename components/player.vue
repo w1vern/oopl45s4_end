0655ad1bec71cdb595fdd0fedb749c232150a38e
dmelackov <dm.elackov@gmail.com>
2024-06-05 19:35:23 +0300

    player cmt

--- components/player.vue
+++ components/player.vue
@@ -1,6 +1,9 @@
 <template>
   <div>
-    <VideoBlock :stream="info.stream" :muted="false"></VideoBlock>
+    <VideoBlock class="block" :stream="info.stream" :muted="false" v-if="!disconnect && !disabled"></VideoBlock>
+    <img class="block" src="~/assets/img/death.png" alt="" srcset="" v-if="!disabled && death">
+    <img class="block" src="~/assets/img/disabled.png" alt="" srcset="" v-if="!disconnect && disabled">
+    <img class="block" src="~/assets/img/dissconect.png" alt="" srcset="" v-if="disconnect && !disabled && !death">
   </div>
 </template>
 
@@ -9,8 +12,23 @@ const props = defineProps({
   info: Object
 })
 
+const death = computed(() => {
+  return !props.info.isAlive;
+})
+
+const disconnect = computed(() => {
+  return info.stream == null
+})
+
+const disabled = computed(() => {
+  return info.sleep;
+})
+
 </script>
 
-<style>
-
+<style scoped>
+.block {
+  width: 100%;
+  height: 100%;
+}
 </style>
